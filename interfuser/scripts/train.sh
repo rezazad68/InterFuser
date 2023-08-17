cf679d6df773a03b63256503e16421dcb988eb16
Reza Azad <rezazad68@gmail.com>
2023-08-17 11:39:09 +0200

    Update train.sh
    lidar is not used

--- interfuser/scripts/train.sh
+++ interfuser/scripts/train.sh
@@ -1,12 +1,12 @@
-GPU_NUM=8
-DATASET_ROOT='dataset/'
+GPU_NUM=1
+DATASET_ROOT='/hdd/datasets/carla_short/'
 
-./distributed_train.sh $GPU_NUM $DATASET_ROOT  --dataset carla --train-towns 1 2 3 4 6 7 10  --val-towns 5 \
-    --train-weathers 0 1 2 3 4 5 6 7 8 9  --val-weathers 10 11 12 13 \
+./distributed_train.sh $GPU_NUM $DATASET_ROOT --dataset carla --train-towns 5 --val-towns 5 \
+    --train-weathers 7  --val-weathers 7 \
     --model interfuser_baseline --sched cosine --epochs 25 --warmup-epochs 5 --lr 0.0005 --batch-size 16  -j 16 --no-prefetcher --eval-metric l1_error \
     --opt adamw --opt-eps 1e-8 --weight-decay 0.05  \
     --scale 0.9 1.1 --saver-decreasing --clip-grad 10 --freeze-num -1 \
     --with-backbone-lr --backbone-lr 0.0002 \
-    --multi-view --with-lidar --multi-view-input-size 3 128 128 \
-    --experiment interfuser_baseline \
+    --multi-view  --multi-view-input-size 3 128 128 \
+    --experiment interfuser_baseline\
     --pretrained
